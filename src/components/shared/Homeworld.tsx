85f64b93eae58b33b4631ff7ee737e27ae56947b
Craig Puxty <craig_puxty@talktalk.net>
2022-10-28 19:29:35 +0100

    Updated Homeworld & Species components.

--- src/components/shared/Homeworld.tsx
+++ src/components/shared/Homeworld.tsx
@@ -10,9 +10,10 @@ import LinkText from "./text/LinkText";
 
 type HomeworldProps = {
   url: string;
+  link?: boolean;
 };
 
-export default function Homeworld({ url }: HomeworldProps) {
+export default function Homeworld({ url, link }: HomeworldProps) {
   const { data: homeworld, isLoading, isError } = useFetchData<IPlanet>(url);
   const href = url?.replace("https://swapi.py4e.com/api", "");
   return (
@@ -21,9 +22,15 @@ export default function Homeworld({ url }: HomeworldProps) {
         <BodyText text="Loading..." />
       ) : isError ? (
         <BodyText text="n/a" />
-      ) : (
+      ) : link ? (
         <LinkText href={href} text={homeworld?.name} />
+      ) : (
+        <BodyText text={homeworld?.name} />
       )}
     </>
   );
 }
+
+Homeworld.defaultProps = {
+  link: false,
+};
--- src/components/shared/Species.tsx
+++ src/components/shared/Species.tsx
@@ -10,9 +10,10 @@ import LinkText from "./text/LinkText";
 
 type SpeciesProps = {
   url: string[];
+  link: boolean;
 };
 
-export default function Species({ url }: SpeciesProps) {
+export default function Species({ url, link }: SpeciesProps) {
   const { data: species, isLoading, isError } = useFetchData<ISpecies>(url[0]);
   const href = url[0]?.replace("https://swapi.py4e.com/api", "");
   return (
@@ -21,9 +22,15 @@ export default function Species({ url }: SpeciesProps) {
         <BodyText text="Loading..." />
       ) : isError ? (
         <BodyText text="n/a" />
-      ) : (
+      ) : link ? (
         <LinkText href={href} text={species?.name} />
+      ) : (
+        <BodyText text={species?.name} />
       )}
     </>
   );
 }
+
+Species.defaultProps = {
+  link: false,
+};
