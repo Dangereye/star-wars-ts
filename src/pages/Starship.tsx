7948fb1ae358a13df4238ce5f829ea12392faf59
Craig Puxty <craig_puxty@talktalk.net>
2022-11-01 22:11:48 +0000

    Updated Starship page. Removed StarshipHeader.

--- src/pages/Starship.tsx
+++ src/pages/Starship.tsx
@@ -1,15 +1,27 @@
 import { useParams } from "react-router-dom";
-import StarshipHeader from "../components/layout/header/StarshipHeader";
-import IsError from "../components/shared/is_error/IsError";
-import IsLoading from "../components/shared/is_loading/IsLoading";
+
+// Hooks
 import useFetchData from "../hooks/useFetchData";
 
+// Components
+import GenericHeader from "../components/layout/header/GenericHeader";
+import IsError from "../components/shared/is_error/IsError";
+import IsLoading from "../components/shared/is_loading/IsLoading";
+
+// Icons
+import { VscRocket } from "react-icons/vsc";
+
+// Interfaces
 import { IStarship } from "../interfaces/starship";
+
+// Utilities
+import StringToStringArray from "../utilities/string_to_string_array/StringToStringArray";
+import CheckUnits from "../utilities/CheckUnits";
 
 export default function Person() {
   const { starshipId } = useParams();
   const {
-    data: starships,
+    data: starship,
     isLoading,
     isError,
   } = useFetchData<IStarship>(
@@ -24,9 +36,54 @@ export default function Person() {
     return <IsError message="Unable to retrieve starships" />;
   }
 
+  const list = [
+    { name: "model", value: starship.model },
+    { name: "starship class", value: starship.starship_class },
+    {
+      name: "manufacturer",
+      value: <StringToStringArray string={starship.manufacturer} />,
+    },
+    {
+      name: "cost in credits",
+      value: <CheckUnits string={starship.cost_in_credits} />,
+    },
+    {
+      name: "length",
+      value: <CheckUnits type="m" string={starship.length} />,
+    },
+    {
+      name: "crew",
+      value: <CheckUnits string={starship.crew} />,
+    },
+    {
+      name: "passengers",
+      value: <CheckUnits string={starship.passengers} />,
+    },
+    {
+      name: "hyperdrive rating",
+      value: <CheckUnits string={starship.hyperdrive_rating} />,
+    },
+    {
+      name: "max speed",
+      value: <CheckUnits type="mglt" string={starship.MGLT} />,
+    },
+    {
+      name: "cargo capacity",
+      value: <CheckUnits type="kg" string={starship.cargo_capacity} />,
+    },
+    {
+      name: "consumables supply",
+      value: <CheckUnits string={starship.consumables} />,
+    },
+  ];
+
   return (
     <>
-      <StarshipHeader data={starships} />
+      <GenericHeader
+        name={starship.name}
+        icon={() => <VscRocket />}
+        list={list}
+      />
     </>
   );
 }
