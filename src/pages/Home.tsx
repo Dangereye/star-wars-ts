0284ad4b3bb7a5a5a0b1f54fd59bb4e393ec7385
Craig Puxty <craig_puxty@talktalk.net>
2022-12-07 23:03:07 +0000

    Updated Home page text.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import BackgroundImage from "../components/shared/background_image/BackgroundImage";
 import Image from "../components/shared/Image";
+import BodyText from "../components/shared/text/BodyText";
 
 export default function Home() {
   return (
@@ -16,6 +16,8 @@ export default function Home() {
               height={500}
               alt="Star Wars Logo"
             />
+
+            <BodyText text="Star Wars data you've always wanted: Planets, Spaceships, Vehicles, People, Films and Species from SEVEN films.  Now with The Force Awakens data!" />
             <Link to="/films" className="btn btn--large btn--primary">
               get started
             </Link>
