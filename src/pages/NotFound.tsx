c5f9bda7e220b659d1c37f027484ec947bcb2ce9
Craig Puxty <craig_puxty@talktalk.net>
2022-12-18 23:38:32 +0000

    Added redirects for netlify. Added basic not found page.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import HDiv from "../components/shared/text/HDiv";
+import BodyText from "../components/shared/text/BodyText";
+
 export default function NotFound() {
   return (
     <main>
-      <h1>Not found.</h1>
+      <article className="article not-found">
+        <div className="container">
+          <HDiv variant="heading--h1" text="oops!" />
+          <BodyText
+            text="Uh, we had a slight weapons malfunction, but uh... everything's
+            perfectly all right now. We're fine"
+          />
+          <div className="buttons">
+            <Link to="/" className="btn btn--large btn--primary">
+              Go home
+            </Link>
+          </div>
+        </div>
+      </article>
     </main>
   );
 }
